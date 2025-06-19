207acb3e5e24da14c4d62fcde50cbe0ff5b5777b
rkudakov <rkudakov@mail.com>
2025-06-19 12:33:55 +0300

    dev

---
+++ rebuild.bat
@@ -0,0 +1,4 @@
+set HTTP_PROXY=http://ubuntu:JtmqcpOxnk98gvsHEASXvx4=@91.147.92.211:30000
+set HTTPS_PROXY=http://ubuntu:JtmqcpOxnk98gvsHEASXvx4=@91.147.92.211:30000
+helm dependency update charts/sdkwa
+helm package charts/sdkwa
